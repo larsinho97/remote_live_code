968bac0de705cabf38f29f28a768664e4001b127
Lars Hirschfelder <lars.hirschfelder@promos-consult.de>
2024-03-26 12:54:21 +0100

    Initial commit

---
+++ src/test/java/com/hirschfelder/MyClassIT.java
@@ -0,0 +1,8 @@
+package com.hirschfelder;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class MyClassIT extends MyClassTest {
+    // Execute the same tests but in packaged mode.
+}
